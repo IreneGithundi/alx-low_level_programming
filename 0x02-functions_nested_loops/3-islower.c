c7b9445ce7538799348a9ee5b8ef21962e855101
IreneGithundi <njokiirene4349@gmail.com>
2024-01-14 02:32:00 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ *_islower  - checks for lowercase
+ *@c: an input character
+ * Return: (1) if lowercase, (0) otherwise
+ */
+
+int _islower(int c)
+{
+	c = 'a';
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
