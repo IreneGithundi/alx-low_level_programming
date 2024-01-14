7225fb24b9ba0c015e70c0a1723b10f6302f6ec3
IreneGithundi <njokiirene4349@gmail.com>
2024-01-14 07:41:25 -0800

    Print Every Minute of ack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * jack_bauer- Prints every minute of Jack Bauer
+ *
+ * Return: null
+ */
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+
+		}
+	}
+}
