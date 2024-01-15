bf6539c4ade3beb03a2f673a2cbd8e6aecee4bcf
IreneGithundi <njokiirene4349@gmail.com>
2024-01-15 00:47:25 -0800

    Print Input Times Table less than or Equal to 15

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,27 +11,24 @@ void print_times_table(int n)
 {
 	int r, c, result;
 
-	if (n <  0 || n > 15)
-		_putchar('\n');
-	else
+	if (n >=  0 && n <= 15)
 	{
 		for (r = 0; r <= n; r++)
 		{
 			_putchar('0');
-			
+
 			for (c = 1; c <= n; c++)
 			{
 				_putchar(',');
 				_putchar(' ');
-				
+
 				result = r * c;
-				
+
 				if (result >= 0 && result < 10)
 				{
 					_putchar(' ');
 					_putchar(' ');
-				}	
-				
+				}
 				if (result >= 10 && result < 100)
 				{
 					_putchar(' ');
@@ -41,7 +38,7 @@ void print_times_table(int n)
 				if (result >= 100)
 				{
 					_putchar((result / 100) + '0');
-					_putchar(((result /10) % 10) + '0');
+					_putchar(((result / 10) % 10) + '0');
 				}
 				_putchar((result % 10) + '0');
 			}
