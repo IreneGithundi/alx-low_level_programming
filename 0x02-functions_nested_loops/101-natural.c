ca00dce2a4f5d072a84baa7ec76d1b7e3cf315d8
IreneGithundi <njokiirene4349@gmail.com>
2024-01-15 01:11:45 -0800

    Compute and Add Natural Numbers below 1024 that are divisible by 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- Adds all natural numbers divisible by 3 & 5
+ *
+ * Return: (0) successful
+ */
+
+int main(void)
+{
+	int x, sum = 0;
+
+	if (((x % 3) = 0) || ((x % 5) = 0))
+	{
+		for (x = 0; x < 1024; x++)
+		{
+			sum = sum + x;
+		}
+		printf("%d\n", sum);
+	}
+}
