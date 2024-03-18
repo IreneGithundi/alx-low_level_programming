0f5ceae2ccc93b173d34a1b42b0520b60e617fca
IreneGithundi <njokiirene4349@gmail.com>
2024-03-18 14:48:48 -0700

    Function that Prints Elements of an Array of Ints

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  *print_array- Print elements of an array of ints
+  * @a: The array input
+  * @n: The number of inputs
+  */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i < n -1)
+			printf(", ");
+	}
+	printf("\n");
+}
