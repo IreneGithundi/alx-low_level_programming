33a1b403d4efca08f69953d52a8f4a11f3f42e68
IreneGithundi <njokiirene4349@gmail.com>
2024-03-20 10:23:01 -0700

    Copying string using the strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * _strncpy- Copying a string
+  * @dest: The destination
+  * @src: The source of the string
+  * @n: The total bytes to be written
+  * Return: Always 1 (A character)
+  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	char *start = dest;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		*dest = src[i];
+		dest++;
+	}
+
+	dest = '\0';
+	return (start);
+}
