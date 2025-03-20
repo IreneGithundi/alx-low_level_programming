83ce4cb5e5981d767d99f8b30c22514d61a4cb8e
Irene <githundiirene@gmail.com>
2025-03-20 02:51:57 -0700

    Three Digit Number Combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a < 8; a++)
+	{
+		for (b = a + 1; b < 9; b++)
+		{
+			for (c = a + 2; c < 10; c++)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
+
+				if (a != 7 || b != 8 || c != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
