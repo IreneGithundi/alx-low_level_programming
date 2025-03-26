3451d8b283b43e50b6582a872a751b86876ed4e1
Irene <githundiirene@gmail.com>
2025-03-25 20:25:37 -0700

    The 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,3 +7,42 @@
  */
 
 void times_table(void)
+{
+	int row;
+
+	row = 0;
+
+	while (row <= 9)
+	{
+		int column;
+
+		column = 0;
+
+		while (column <= 9)
+		{
+			int product;
+
+			product = row * column;
+
+			if (product <= 9)
+			{
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+
+			else if (product > 9)
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+				if (row <= 9 && column != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			column++;
+		}
+		_putchar('\n');
+		row++;
+	}
+}
