32b34b91f4e3e34324f1b5ad0a6b159ea0337379
Irene <githundiirene@gmail.com>
2025-04-17 08:15:04 -0700

    Print Numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -26,7 +26,7 @@ void print_number(int n)
 		else if (n >= 100 && n <= 999)
 		{
 			_putchar((n / 100) + '0');
-			_putchar(((n / 100) % 10) + '0');
+			_putchar(((n / 10) % 10) + '0');
 			_putchar((n % 10) + '0');
 		}
 		else if (n >= 1000 && n <= 9999)
