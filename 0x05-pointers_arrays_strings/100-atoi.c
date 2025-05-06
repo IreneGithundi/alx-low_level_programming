f5a7d01367b9e37c9b9a1b22308e94200aa6f49b
Irene <githundiirene@gmail.com>
2025-05-06 11:27:27 -0700

    Pointer Copy

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * _atoi - Convert a string to an integer
+  * *s: string to be converted to an integer
+  *
+  * Return: (int) the numbers in a string
+  */
+
+int _atoi(char *s)
+{
+	int i;
+
+	i = 0;
+
+}
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,12 +8,15 @@
   */
 char *_strcpy(char *dest, char *src)
 {
-	while (*src <= '\0');
-	{
-		*dest = *src;
-		*src++;
-		*dest++;
-	}
+	int i;
 
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
