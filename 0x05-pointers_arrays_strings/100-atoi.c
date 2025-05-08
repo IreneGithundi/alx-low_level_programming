d4b115701838135a701a608c2443dd477fa30715
Irene <githundiirene@gmail.com>
2025-05-07 20:48:14 -0700

    Convert a Charcater to a String

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,15 +2,38 @@
 
 /**
   * _atoi - Convert a string to an integer
-  * *s: string to be converted to an integer
+  * s: string to be converted to an integer
   *
   * Return: (int) the numbers in a string
   */
 
 int _atoi(char *s)
 {
-	int i;
+	int i, sign, result;
 
 	i = 0;
+	sign = 1;
+	result = 0;
 
+	while (s[i] && (s[i] < '0' || s[i] > '9') && (s[i] != '-' && s[i] != '+'))
+	{
+		i++;
+	}
+
+	while (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		i++;
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (result * sign);
 }
