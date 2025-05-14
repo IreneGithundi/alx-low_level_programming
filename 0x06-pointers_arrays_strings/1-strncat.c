1726bd3775832b59aee97845d41d75da6033256f
Irene <githundiirene@gmail.com>
2025-05-14 08:59:44 -0700

    Concatenate N bytes to dest

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * _strncat - Function that concatenates 2 strings n bytes from src to dest
+  * @dest: the string that is added to from src
+  * @src: the string that is being copied to dest
+  * @n: The number of bytes that's to be copied
+  *
+  * Return: (dest) now concatenated
+  */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index, i;
+
+	index = 0;
+	i = 0;
+
+	while (dest[index] != '\0')
+	{
+		index++;
+	}
+
+	while (i < n)
+	{
+		dest[index] = src[i];
+		i++;
+		index++;
+	}
+
+	dest[index] = '\0';
+
+	return (dest);
+}
